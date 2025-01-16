5628e3a80434edd1367a3f2bcbdc79523072e12a
TextualMite9145 <steinmannale@student.carey.wa.edu.au>
2025-01-16 14:26:42 +0800

    Initial commit

---
+++ src/main/java/net/alex/hugemod/HugeModClient.java
@@ -0,0 +1,10 @@
+package net.alex.hugemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HugeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
