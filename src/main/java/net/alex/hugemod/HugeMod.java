7c3cdf8c7633607ab526df549ee1a2302f43363a
TextualMite9145 <steinmannale@student.carey.wa.edu.au>
2025-01-16 14:27:33 +0800

    added a comment

--- src/main/java/net/alex/hugemod/HugeMod.java
+++ src/main/java/net/alex/hugemod/HugeMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//I agree, very important
 public class HugeMod implements ModInitializer {
 	public static final String MOD_ID = "hugemod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
